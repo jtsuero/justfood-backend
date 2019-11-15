79af9b23c5cd07e51d361058507b29cad2c3255d
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-11-14 19:52:04 -0800

    add yelp-fusion plugin and dependecies

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
+const yelp = require('yelp-fusion');
+const API_key = r1vbEMtp8IBHtXAGam3bzbj7UTc3yEfmd6tRnfcc7L14ezrh-HlwjfSAefD6pNYa1vPHGKyPHvXL4uSIapALyPuvTE9PXCEYpofz4nnNj7YXNJZTYyyaLPanDB7OXXYx;
+const client = yelp.client(API_key);
 
 app.listen(3000, () => console.log('Server up and running'));
