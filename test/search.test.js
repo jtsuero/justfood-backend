03eed00cd0135ec6678fc64af92069037bf4f476
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-11-18 20:42:45 -0800

    make test actually work

--- test/search.test.js
+++ test/search.test.js
@@ -1,13 +1,17 @@
 const request = require('supertest');
 const app = require('../index.js');
+const assert = require('chai').assert;
 
 describe("search", function(done) {
-  it("returns json list of businesses based on long and lat", function(done) {
-    request(app).get("/search/?long=-122.335167&lat=47.608013&radius=8000")
+  it("returns json list of businesses based on long and lat", function() {
+    return request(app).get("/search/?long=-122.335167&lat=47.608013&radius=8000")
       .expect(200)
       .expect(function(res) {
-        res.body.businesses[0].id = "6I28wDuMBR5WLMqfKxaoeg";
+        assert.equal(res.body.businesses.length, 20)
+        res.body.businesses.forEach((business) => {
+          assert.isString(business.id)
+          assert.isString(business.name)
+        })
       })
-      .expect('Content-Type', /json/, done)
   });
 })
