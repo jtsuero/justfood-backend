9f60cc43edda456972cfab4d480a8a2832187e1c
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-11-20 22:38:46 -0800

    update test to account for rate limit, and add expect requirement

--- test/search.test.js
+++ test/search.test.js
@@ -1,20 +1,22 @@
 const request = require('supertest');
 const app = require('../index.js');
 const assert = require('chai').assert;
+const expect = require('chai').expect;
+const resultsNumber = 5;
 
 describe("search", function(done) {
   it("returns json list of businesses based on long and lat", function() {
-    return request(app).get("/search/?long=-122.335167&lat=47.608013&radius=8000&term=food")
+    return request(app).get(`/search/?long=-122.335167&lat=47.608013&radius=8000&term=food&limit=${resultsNumber}`)
       .expect(200)
       .expect(function(res) {
-        assert.equal(res.body.businesses.length, 20)
+        assert.equal(res.body.businesses.length, resultsNumber)
         res.body.businesses.forEach((business) => {
           assert.isString(business.id)
           assert.isString(business.name)
           assert.isString(business.yelp_url)
+          assert.isArray(business.hours)
+          expect(business.photos).to.have.lengthOf(3)
         })
       })
-      .expect(business.photos.to.have.lengthOf(3))
-      .expect(business.hours.to.have.lengthOf(7))
   });
 })
