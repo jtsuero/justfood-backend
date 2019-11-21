16dce54bce267a5c38f045c1789544ee0d73822c
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-11-20 22:44:51 -0800

    get photos and hours for restaurants

--- routes/search.js
+++ routes/search.js
@@ -12,29 +12,40 @@ router.get('/', async (req, res) => {
       term: req.query.term,
       latitude: req.query.lat,
       longitude: req.query.long,
-      radius: req.query.radius
-    }).then(yelpResponse => {
-      const restaurants = transformRestaurants(yelpResponse.jsonBody.businesses);
+      radius: req.query.radius,
+      limit: req.query.limit,
+    })
+    .then(yelpResponse => {
+      transformRestaurants(yelpResponse.jsonBody.businesses)
+    .then(restaurants => {
       res.status(200).json({
-        businesses: restaurants,
+      businesses: restaurants,
       });
+    });
     }).catch(e => {
       res.status(400).json(e);
     });
-
   } catch (err) {
     res.status(400).json({message: err});
   }
   });
 
   function transformRestaurants(yelpRestaurants) {
-    return yelpRestaurants.map((restaurant) => {
-      return {
-        id: restaurant.id,
-        name: restaurant.name,
-        yelp_url: restaurant.url,
-      };
+    const businessRequests = yelpRestaurants.map((restaurant) => {
+      return client.business(restaurant.id)
+      .then(response => {
+          return {
+            id: response.jsonBody.id,
+            name: response.jsonBody.name,
+            yelp_url: response.jsonBody.url,
+            photos: response.jsonBody.photos,
+            hours: response.jsonBody.hours,
+          };
+      }).catch(e => {
+        console.log(e);
+      });
     })
+    return Promise.all(businessRequests)
   }
 
 module.exports = router;
