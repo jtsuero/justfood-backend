d9836b7db2d3321967f6fde62b7cecd9e2e5bd05
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-11-24 11:02:46 -0800

    add const terms for search params

--- test/places.test.js
+++ test/places.test.js
@@ -3,10 +3,14 @@ const app = require('../index.js');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const resultsNumber = 5;
+const longitude =  -122.335167;
+const latitude = 47.608013;
+const searchTerm = "food";
+const radius = 8000;
 
 describe("search", function(done) {
   it("returns json list of businesses based on long and lat", function() {
-    return request(app).get(`/search/?long=-122.335167&lat=47.608013&radius=8000&term=food&limit=${resultsNumber}`)
+    return request(app).get(`/search/?long=${longitude}&lat=${latitude}&radius=${radius}&term=${searchTerm}&limit=${resultsNumber}`)
       .expect(200)
       .expect(function(res) {
         assert.equal(res.body.businesses.length, resultsNumber)
